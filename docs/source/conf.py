3dbb69c6be4f11bf80381d2195f8490d91c4328e
dietervansteenwegen <github@vansteenwegen.org>
2022-03-03 11:21:30 +0100

    Corrected version info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ project = 'Panthyr GPIO'
 author = 'Dieter Vansteenwegen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1b'
+release = '0.1.0-beta'
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -2,4 +2,6 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='p_gpio', version='1.0', packages=find_packages())
+setup(name='p_gpio',
+      version='0.1.0-beta',
+      packages=find_packages())
