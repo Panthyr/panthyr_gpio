7a06c217721fcf1544a0bf49fd9966ba007139a0
dietervansteenwegen <github@vansteenwegen.org>
2023-05-08 11:41:23 +0200

    preparation for release v1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ project = 'Panthyr GPIO'
 author = 'Dieter Vansteenwegen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = 'v1.0.0'
 
 # -- General configuration ---------------------------------------------------
 
